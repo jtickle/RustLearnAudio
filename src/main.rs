4dad4703dc74fb1a103483f8a05d32f2d15ec22a
Jeff Tickle <jeff.tickle@protonmail.com>
2022-11-13 15:37:42 -0500

    Brought in code from cpal feedback example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+// Strongly inspired by, and some sections totally ripped off from:
+// https://github.com/RustAudio/cpal/blob/master/examples/feedback.rs
+
 use cpal;
-use cpal::traits::{DeviceTrait, HostTrait};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use ringbuf::HeapRb;
 
 macro_rules! display_whatevers {
     ($fn: expr, $and_then: ident, $prefix: expr, $or_error: expr) => {
@@ -31,10 +35,12 @@ fn display_device(device: &cpal::Device, prefix: &str, _count: u32) {
     let npfx2 = &format!("{}    ", prefix);
 
     println!("{}Input Configs:", npfx);
+
     display_whatevers!(device.supported_input_configs(), display_config, npfx2,
         format!("Error retrieving input configs for device {}", dev_name));
 
     println!("{}Output Configs:", npfx);
+
     display_whatevers!(device.supported_output_configs(), display_config, npfx2,
             format!("Error retrieving output configs for device {}", dev_name));
 }
@@ -62,4 +68,89 @@ fn display_capabilities() {
 
 fn main() {
     display_capabilities();
+
+    // Define latency
+    let latency:f32 = 1000.0;
+
+    let host = cpal::default_host();
+    let input_device = host.default_input_device().unwrap();
+    let output_device = host.default_output_device().unwrap();
+
+    println!("Using input device: \"{}\"", input_device.name().unwrap());
+    println!("Using output device: \"{}\"", output_device.name().unwrap());
+
+    // Try using the same configuration between streams to keep it simple
+    let config: cpal::StreamConfig = input_device.default_input_config().unwrap().into();
+
+    // Create a delay in case input and output devices aren't synced
+    let latency_frames = (latency / 1_000.0) * config.sample_rate.0 as f32;
+    let latency_samples = latency_frames as usize * config.channels as usize;
+
+    // Create the buffer to share samples
+    let ring = HeapRb::<f32>::new(latency_samples * 2);
+    let (mut producer, mut consumer) = ring.split();
+
+    // Fill the samples with 0.0 equal to the length of the delay.
+    for _ in 0..latency_samples {
+        // The ring buffer has twice as much space as necessary to add latency here,
+        // so this should never fail
+        producer.push(0.0).unwrap();
+    }
+
+    let input_data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
+        let mut output_fell_behind = false;
+        for &sample in data {
+            if producer.push(sample).is_err() {
+                output_fell_behind = true;
+            }
+        }
+        if output_fell_behind {
+            eprintln!("output stream fell behind: try increasing latency");
+        }
+    };
+
+    let output_data_fn = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+        let mut input_fell_behind = false;
+        for sample in data {
+            *sample = match consumer.pop() {
+                Some(s) => s,
+                None => {
+                    input_fell_behind = true;
+                    0.0
+                }
+            };
+        }
+        if input_fell_behind {
+            eprintln!("input stream fell behind: try increasing latency");
+        }
+    };
+
+    // Build streams.
+    println!(
+        "Attempting to build both streams with f32 samples and `{:?}`.",
+        config
+    );
+    let input_stream = input_device.build_input_stream(&config, input_data_fn, err_fn).unwrap();
+    let output_stream = output_device.build_output_stream(&config, output_data_fn, err_fn).unwrap();
+    println!("Successfully built streams.");
+
+    // Play the streams.
+    println!(
+        "Starting the input and output streams with `{}` milliseconds of latency.",
+        latency
+    );
+    input_stream.play().unwrap();
+    output_stream.play().unwrap();
+
+    // Run for 3 seconds before closing
+    println!("Playing for 3 seconds...");
+    std::thread::sleep(std::time::Duration::from_secs(3));
+
+    drop(input_stream);
+    drop(output_stream);
+    println!("Done!");
+}
+
+fn err_fn(err: cpal::StreamError) {
+    eprintln!("an error occurred on stream: {}", err);
 }
